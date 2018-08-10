dc98f03eca9caf8636255a8ffeeda8485f168789
Nick Ho <nick133371@gmail.com>
2018-08-10 11:03:10 +0800

    6

--- xos/synchronizer/steps/sync_oaibbutenant.py
+++ xos/synchronizer/steps/sync_oaibbutenant.py
@@ -41,7 +41,40 @@ class SyncOAIBBUTenant(SyncInstanceUsingAnsible):
     def __init__(self, *args, **kwargs):
         super(SyncOAIBBUTenant, self).__init__(*args, **kwargs)
 
+    def get_oaibbuservice(self, o):
+        if not o.owner:
+            return None
+
+        oaibbuservice = OAIBBUService.objects.filter(id=o.owner.id)
+
+        if not oaibbuservice:
+            return None
+
+        return oaibbuservice[0]
+
+    # Gets the attributes that are used by the Ansible template but are not
+    # part of the set of default attributes.
     def get_extra_attributes(self, o):
-        fields = self.get_information(o)
+        fields = {}
+        fields['tenant_message'] = o.tenant_message
+        oaibbuservice = self.get_oaibbuservice(o)
+        fields['service_message'] = oaibbuservice.service_message
+
+        if o.foreground_color:
+            fields["foreground_color"] = o.foreground_color.html_code
+
+        if o.background_color:
+            fields["background_color"] = o.background_color.html_code
+
+        images=[]
+        for image in o.embedded_images.all():
+            images.append({"name": image.name,
+                           "url": image.url})
+        fields["images"] = images
 
         return fields
+
+    def delete_record(self, port):
+        # Nothing needs to be done to delete an oaibbuservice; it goes away
+        # when the instance holding the oaibbuservice is deleted.
+        pass
